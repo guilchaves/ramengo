f1fc3f6ad4031bf7d2179b40d8a55a997bc9f2a2
guilherme chaves <guilherme.chaves@simplasolucoes.com.br>
2024-06-02 23:24:32 -0300

    feat: add protein service

--- src/main/java/br/com/guilchaves/ramengo/service/ProteinService.java
+++ src/main/java/br/com/guilchaves/ramengo/service/ProteinService.java
@@ -1,4 +1,22 @@
 package br.com.guilchaves.ramengo.service;
 
+import br.com.guilchaves.ramengo.entities.Protein;
+import br.com.guilchaves.ramengo.repository.ProteinRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
 public class ProteinService {
+
+    @Autowired
+    private ProteinRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<Protein> findAll(){
+        return repository.findAll();
+    }
+
 }
