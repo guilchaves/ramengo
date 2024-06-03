878c88466b81cbb010cd16a116e914c81d6c8063
guilherme chaves <guilherme.chaves@simplasolucoes.com.br>
2024-06-02 23:04:04 -0300

    feat: add OrderRequest entity

---
+++ src/main/java/br/com/guilchaves/ramengo/entities/OrderRequest.java
@@ -0,0 +1,63 @@
+package br.com.guilchaves.ramengo.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order_request")
+public class OrderRequest implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 4528359399475036453L;
+
+    @OneToOne
+    @JoinColumn(name = "broth_id")
+    private Broth broth;
+
+    @OneToOne
+    @JoinColumn(name = "protein_id")
+    private Protein protein;
+
+    public Broth getBroth() {
+        return broth;
+    }
+
+    public void setBroth(Broth broth) {
+        this.broth = broth;
+    }
+
+    public Protein getProtein() {
+        return protein;
+    }
+
+    public void setProtein(Protein protein) {
+        this.protein = protein;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderRequest that = (OrderRequest) o;
+        return Objects.equals(broth, that.broth) && Objects.equals(protein, that.protein);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(broth, protein);
+    }
+
+    @Override
+    public String toString() {
+        return "OrderRequest{" +
+                "broth=" + broth +
+                ", protein=" + protein +
+                '}';
+    }
+}
