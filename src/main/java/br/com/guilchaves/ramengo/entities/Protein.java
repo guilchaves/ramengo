3ee8c50f474f686115288bde241e69d63da928a2
guilherme chaves <guilherme.chaves@simplasolucoes.com.br>
2024-06-02 22:53:46 -0300

    feat: add protein entity

--- src/main/java/br/com/guilchaves/ramengo/entities/Protein.java
+++ src/main/java/br/com/guilchaves/ramengo/entities/Protein.java
@@ -1,4 +1,69 @@
 package br.com.guilchaves.ramengo.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_protein")
 public class Protein {
+
+    @Id
+    private String id;
+    private String imageInactive;
+    private String imageActive;
+    private String name;
+    private String description;
+    private Double price;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getImageInactive() {
+        return imageInactive;
+    }
+
+    public String getImageActive() {
+        return imageActive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Protein protein = (Protein) o;
+        return Objects.equals(id, protein.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Protein{" +
+                "id='" + id + '\'' +
+                ", imageInactive='" + imageInactive + '\'' +
+                ", imageActive='" + imageActive + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
