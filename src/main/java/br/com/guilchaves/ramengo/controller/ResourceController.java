bb395de48c6906fd0798f4f3e4c9d3ebfcab64fe
guilherme chaves <guilherme.chaves@simplasolucoes.com.br>
2024-06-03 00:31:44 -0300

    feat: add security with x-api-key header to requests

---
+++ src/main/java/br/com/guilchaves/ramengo/controller/ResourceController.java
@@ -0,0 +1,12 @@
+package br.com.guilchaves.ramengo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResourceController {
+    @GetMapping("/home")
+    public String homeEndpoint() {
+        return "Baeldung!";
+    }
+}
