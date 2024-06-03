025414c884d365fb39fc34dc4203f1c100fb213b
guilherme chaves <guilherme.chaves@simplasolucoes.com.br>
2024-06-02 23:49:34 -0300

    feat: add broth repository

---
+++ src/main/java/br/com/guilchaves/ramengo/repository/BrothRepository.java
@@ -0,0 +1,10 @@
+package br.com.guilchaves.ramengo.repository;
+
+import br.com.guilchaves.ramengo.entities.Broth;
+import br.com.guilchaves.ramengo.entities.Protein;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrothRepository extends JpaRepository<Broth, String> {
+}
